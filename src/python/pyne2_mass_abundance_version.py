4283edc760af4712ec699762f41ee6213c476b09
Paul P.H. Wilson <paul.wilson@wisc.edu>
2022-02-19 11:44:22 -0600

    updated to automated version from new stub

--- src/python/pyne2_mass_abundance_version.py
+++ src/python/pyne2_mass_abundance_version.py
@@ -2,13 +2,12 @@ import sys
 sys.path.append("../../config")
 import write_common_headers
 
-# NMA for nuclide mass & abundance
-P2_NMA_NAME = 'mass_abundance'
-P2_NMA_MAJOR_VERSION = 0
-P2_NMA_MINOR_VERSION = 1
-P2_NMA_PATCH_VERSION = 0
+P2_NAME = '_'.join(__file__.split('_')[1:-1])
+MAJOR_VERSION = 0
+MINOR_VERSION = 1
+PATCH_VERSION = 0
 
-stub_version = write_common_headers.write_version_header(P2C_NAME,
-                                                         P2_NMA_MAJOR_VERSION,
-                                                         P2_NMA_MINOR_VERSION,
-                                                         P2_NMA_PATCH_VERSION)
+stub_version = write_common_headers.write_version_header(P2_NAME,
+                                                         MAJOR_VERSION,
+                                                         MINOR_VERSION,
+                                                         PATCH_VERSION)
